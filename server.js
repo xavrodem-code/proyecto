ec4a9290a233dfbfea53006fcfe59e42f0aa19ce
xavrodem-code <77444931+xavrodem-code@users.noreply.github.com>
2023-05-01 12:53:04 +0100

    server routes updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const app = express();
 
 app.use(express.json());
