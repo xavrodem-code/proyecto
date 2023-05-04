16dfc029cb5770767057025f77beb96f366f46bc
xavrodem-code <77444931+xavrodem-code@users.noreply.github.com>
2023-05-04 12:36:47 +0100

    login actualizado

--- routes/usuariosRoute.js
+++ routes/usuariosRoute.js
@@ -128,6 +128,7 @@ router.post("/login", async (req, res, next) => {
   res.status(201).json({
     mensaje: "Usuario ha entrado en el sistema con éxito",
     email: usuarioExiste.email,
+    idUsuario: usuarioExiste.id,
     token: token,
   });
 });
