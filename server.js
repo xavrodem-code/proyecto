9ffef9e611d60b2e6b31262808e990f501263cff
xavrodem-code <77444931+xavrodem-code@users.noreply.github.com>
2023-05-05 12:04:42 +0100

    cors origin update

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cors());
 
-app.use("/api/usuarios/", require("./routes/usuariosRoute"));
-app.use("/api/fechas/", require("./routes/fechasRoute"));
+app.use("/api/usuarios", require("./routes/usuariosRoute"));
+app.use("/api/fechas", require("./routes/fechasRoute"));
 
 app.use((req, res) => {
   res.status(404);
