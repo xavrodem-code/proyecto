393b7f23d291112e837584df887a76a460c8cfa4
xavrodem-code <77444931+xavrodem-code@users.noreply.github.com>
2023-05-05 13:09:53 +0100

    response updated

--- routes/fechasRoute.js
+++ routes/fechasRoute.js
@@ -21,7 +21,7 @@ router.get("/", async (req, res, next) => {
   });
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/registrar", async (req, res, next) => {
   const { fecha, usuario } = req.body;
   let buscarUsuario;
   let existeFecha;
